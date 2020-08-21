7d67a6c022159619dc1e8e2af1e455a04175ead8
sunzhaoc <sunzhaoc_2011@163.com>
2020-08-21 08:21:10 +0800

    master

---
+++ test.py
@@ -0,0 +1,15 @@
+'''
+Description: 
+Version: 1.0
+Autor: Vicro
+Date: 2020-08-21 08:13:38
+LastEditors: Vicro
+LastEditTime: 2020-08-21 08:15:23
+'''
+import matplotlib.pyplot as plt
+
+input = open('test.txt', 'r')
+
+rangeUpdateTime = [0.0]
+
+a=1
